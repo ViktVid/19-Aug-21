22bf69d832b385b7edf1edbc6ee31f32ab553b32
ViktVid <videnovicviktoria@gmail.com>
2021-11-13 20:14:06 +0100

    Via Git Bash

---
+++ pack19_08/PetljaFor.java
@@ -0,0 +1,131 @@
+package pack19_08;
+
+import java.util.Scanner;
+
+public class PetljaFor {
+
+	public static void main(String[] args) {
+		
+	/*	for (int i = 0; i < 5; i++) {
+			System.out.println("i: " + i);
+		}
+		for (int i = 0; i < 100; i = i + 5) {
+			System.out.println("i: " + i);
+		} */
+		//ZADATAK: Izracunati sumu prvih n brojeva
+
+/*		Scanner nBroj = new Scanner(System.in);
+		System.out.println("Unesite broj do kojeg radi sumiranje: ");
+
+		int n = nBroj.nextInt();
+		int suma = 0;
+
+		for (int x = 0; x < n; x++) {
+			suma = suma + x;
+		}
+
+		System.out.println("Rezultat je: " + suma); */
+		
+		
+/* for (int i = 0; i >= 0; i++) {
+		system.out.println("i: " + i);
+		} 
+		
+		for (int i = 100; i >= 0; i--) {
+		system.out.println("i: " + i);
+		}
+		
+		for (int i = 0; i >= 10; i--) {
+			for(int j = 0; j < 10; j++);
+		} */
+		//String.length() < broj
+/* 
+ 1. Koristeci dve for petlje nacrtati
+	**********
+	**********
+	**********
+	**********
+	**********
+	**********
+	**********
+	**********
+	**********
+	********** 
+	
+*/	
+
+/* 2. Koristec dve for petlje nacrtati
+	
+	*
+	**
+	***
+	****
+	*****
+	******
+	*******
+	********
+	*********
+	**********
+	for (int i = 0; i < 10; i++) {
+			System.out.println("*");
+			for ( int z = 0; z<=i; z++) {
+				System.out.println("*");
+	*/
+	//	Scanner zvezda = new Scanner(System.in);
+		
+		
+/*
+ 	for (int i = 0; i < 10; i++) {
+		System.out.println("*");
+			for ( int z = 0; z < 10; z++) {
+			System.out.println("*");
+				*/
+				
+/*			
+		for (int i = 0; i < 10; i++) {
+			System.out.println("*");
+			for (int z = 0; z < 10; z++) {
+				System.out.println("*");
+			}
+			System.out.println();
+			
+			
+		}
+	}
+} */
+
+
+
+
+		/* String z = "*";
+		for (int i = 0; i < 5; i++) {
+			for (int x = 0; x < i; x++) {
+				System.out.println(z);
+				z = z + "*"; */
+/*		for (int i = 0; i < 3; i++) {
+			System.out.println("i: " + i);
+			for (int j = 0; j < i; j++) {
+				System.out.println("   j: " + j);
+			}				*/
+		
+	/*	for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		} */
+		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+
+
+
+
